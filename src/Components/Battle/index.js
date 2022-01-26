7332c502d7856a1d6eb050f7a3b786745acc2e58
Himanshu <arorahimanshu472@gmail.com>
2022-01-26 20:17:00 +0530

    Battle form added

--- src/Components/Battle/index.js
+++ src/Components/Battle/index.js
@@ -1,5 +1,6 @@
 import cx from "classnames";
 import styles from './styles.module.css';
+import { useState } from "react";
 import {FaTrophy,FaUserFriends,FaFighterJet} from 'react-icons/fa';
 
 function Instruction() {
@@ -24,10 +25,47 @@ function Instruction() {
     )
 }
 
+function Player({label}) {
+    const [user,setUser] = useState('');
+
+    return(
+        <>
+            <form className={cx(styles.player)} onSubmit={() => console.log('here')}>
+                <label className={cx(styles.player_label)}>
+                    {label}
+                </label>
+                <div className={cx(styles.player_input)}>
+                    <input 
+                        type="text" 
+                        value = {user} 
+                        onChange={(e)=> setUser(e.target.value)}
+                        placeholder="github user"
+                    />
+                    <button 
+                        className={cx(styles.btn,styles.dark_btn)} 
+                        type="submit"
+                        disabled = {!user}
+                    >
+                            Submit
+                        </button>
+                </div>
+            </form>
+        </>
+    )
+}
+
 function Battle() {
     return (
         <>
             <Instruction />
+            <div className={cx(styles.players)}>
+                <h1 className={cx(styles.h1)}>Players</h1>
+                <div className={cx(styles.players_group)}>
+                    <Player label="Player 1" />
+                    <Player label="Player 2" />
+                </div>
+            </div>
+            
         </>
     )
 }
